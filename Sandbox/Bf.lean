58fb94eb073b01ab7060ba19da9f83c82fae6153
James Oswald <jamesoswald111@gmail.com>
2024-07-25 05:04:49 -0400

    refactor: Comment out unused code in Machine.step function

--- Sandbox/Bf.lean
+++ Sandbox/Bf.lean
@@ -48,39 +48,47 @@ lemma a_lt_b_imp_a_pred_lt_b (a b : Nat) : a < b -> a - 1 < b := by
     omega
 
 --Step function, returns the new machine state after executing the current instruction
-def Machine.step (m : Machine) : Machine :=
-  if H : m.prog_pos.val < m.prog.length then --End of program
-    match m.prog.get ⟨m.prog_pos.val, H⟩ with
-    | '>' => {m with
-      tape := m.tape ++ [0],
-      tape_pos := ⟨m.tape_pos.val + 1, by simp [m.tape_pos.isLt, Nat.add_lt_add_right]⟩,
-      prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
-    | '<' => {m with
-      tape_pos := ⟨m.tape_pos.val - 1, by simp [a_lt_b_imp_a_pred_lt_b]⟩,
-      prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
-    | '+' => {m with
-      tape := m.tape.set m.tape_pos ((m.tape.get m.tape_pos) + 1),
-      tape_pos := ⟨m.tape_pos, by simp [List.length_set]⟩,
-      prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
-    | '-' => {m with
-      tape := m.tape.set m.tape_pos ((m.tape.get m.tape_pos) - 1),
-      tape_pos := ⟨m.tape_pos, by simp [List.length_set]⟩,
-      prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
-    | '.' => {m with
-      output := m.output.append [Char.ofNat (m.tape.get m.tape_pos)],
-      prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
-    | ',' => {m with
-      tape := m.tape.set m.tape_pos (m.input.headD (Char.ofNat (m.tape.get m.tape_pos))).toNat,
-      tape_pos := ⟨m.tape_pos, by simp [List.length_set]⟩,
-      input := m.input.tailD []
-      prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
-    | '[' => {m with
-      stack := m.stack.append [⟨m.prog_pos, by simp [H]⟩],
-      prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
+-- def Machine.step (m : Machine) : Machine :=
+--   if H : m.prog_pos.val < m.prog.length then --End of program
+--     match m.prog.get ⟨m.prog_pos.val, H⟩ with
+--     | '>' => {m with
+--       tape := m.tape ++ [0],
+--       tape_pos := ⟨m.tape_pos.val + 1, by simp [m.tape_pos.isLt, Nat.add_lt_add_right]⟩,
+--       prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
+--     | '<' => {m with
+--       tape_pos := ⟨m.tape_pos.val - 1, by simp [a_lt_b_imp_a_pred_lt_b]⟩,
+--       prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
+--     | '+' => {m with
+--       tape := m.tape.set m.tape_pos ((m.tape.get m.tape_pos) + 1),
+--       tape_pos := ⟨m.tape_pos, by simp [List.length_set]⟩,
+--       prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
+--     | '-' => {m with
+--       tape := m.tape.set m.tape_pos ((m.tape.get m.tape_pos) - 1),
+--       tape_pos := ⟨m.tape_pos, by simp [List.length_set]⟩,
+--       prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
+--     | '.' => {m with
+--       output := m.output.append [Char.ofNat (m.tape.get m.tape_pos)],
+--       prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
+--     | ',' => {m with
+--       tape := m.tape.set m.tape_pos (m.input.headD (Char.ofNat (m.tape.get m.tape_pos))).toNat,
+--       tape_pos := ⟨m.tape_pos, by simp [List.length_set]⟩,
+--       input := m.input.tailD []
+--       prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
+--     | '[' =>
+--     -- if m.tape.get m.tape_pos = 0 then
+--     --   let loop_end := m.stack.headD ⟨0, by simp⟩
+--     --   {m with
+--     --   stack := m.stack.tailD [],
+--     --   prog_pos := ⟨loop_end.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
+--     -- else
+--     --   {m with
+--     --   stack := m.stack.append [m.prog_pos],
+--     --   prog_pos := ⟨m.prog_pos.val + 1, by simp [H, Nat.add_lt_add_right]⟩}
 
-    | _ => m
-  else
-    m
+
+--     | _ => m
+--   else
+--     m
 
 --BF hello world program
 def HelloWorld : String := "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
